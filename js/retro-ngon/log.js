f13208301486180f756a6f2c198162ca6f20c078
leikareipa <drilledicecream@gmail.com>
2019-02-08 22:15:31 +0200

    Add custom logger barebone
    Basically a roundabout for console.log() at the moment, but saves
    manually appending "Rngon" to each message.

---
+++ js/retro-ngon/log.js
@@ -0,0 +1,14 @@
+/*
+ * Tarpeeksi Hyvae Soft 2019 /
+ * Retro n-gon renderer
+ * 
+ * Provides convenience functions for logging messages.
+ * 
+ */
+
+"use strict"
+
+Rngon.log = function(message = "Hello there.")
+{
+    console.log("Rngon:", message);
+}
