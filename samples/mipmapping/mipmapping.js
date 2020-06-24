bd1cc5b2ec33a1f77cdde2c8dd4437073fa4d20a
leikareipa <drilledicecream@gmail.com>
2020-06-24 16:18:43 +0300

    samples/mipmapping: Let user adjust the mipmapping level

--- samples/mipmapping/mipmapping.js
+++ samples/mipmapping/mipmapping.js
@@ -15,7 +15,7 @@ export const sample_scene = (frameCount = 0)=>
 {
     return Rngon.mesh(scene.ngons,
     {
-        translation: Rngon.translation_vector(-80, -40, (200 + Math.sin(frameCount/100) * 340)),
+        translation: Rngon.translation_vector(-80, -40, (200 + Math.cos(frameCount/100) * 340)),
         rotation: Rngon.rotation_vector(0, -90, 0),
         scaling: Rngon.scaling_vector(25, 25, 45)
     });
@@ -26,13 +26,16 @@ export const sampleRenderOptions = {
     {
         if (ngon.material.hasMipmapping)
         {
+            const minLevel = ((parent.MIPMAP_LEVEL === "Auto")? 0.0 : parent.MIPMAP_LEVEL);
+            const maxLevel = ((parent.MIPMAP_LEVEL === "Auto")? 0.4 : parent.MIPMAP_LEVEL);
+
             const maxDistance = (700 * 700)
 
             const distance = (((ngon.vertices[0].x - cameraPosition.x) * (ngon.vertices[0].x  - cameraPosition.x)) +
                               ((ngon.vertices[0].y - cameraPosition.y) * (ngon.vertices[0].y  - cameraPosition.y)) +
                               ((ngon.vertices[0].z - cameraPosition.z) * (ngon.vertices[0].z  - cameraPosition.z)));    
 
-            ngon.mipLevel = Math.max(0, Math.min(0.5, (distance / maxDistance)));
+            ngon.mipLevel = Math.max(minLevel, Math.min(maxLevel, (distance / maxDistance)));
         }
     }
 }
