ca5aa97121ddc395926599f3565226e6e0af5a59
leikareipa <drilledicecream@gmail.com>
2020-08-16 14:33:45 +0300

    samples: Add a ray-traced lighting sample

---
+++ samples/raytraced-lighting/ray.js
@@ -0,0 +1,123 @@
+/*
+ * 2019, 2020 Tarpeeksi Hyvae Soft
+ *
+ * Software: Retro n-gon renderer
+ * 
+ * Provides a ray object for a ray tracer.
+ * 
+ */
+
+"use strict";
+
+export function ray(pos = Rngon.vector3(0, 0, 0), dir = Rngon.vector3(0, 0, 1))
+{
+    const publicInterface =
+    {
+        pos,
+        dir,
+
+        // Returns the ray's distance to its corresponding intersection point on the given
+        // triangle; or null if the ray doesn't intersect the triangle. Adapted from Moller
+        // & Trumbore 1997: "Fast, minimum storage ray/triangle intersection".
+        intersect_triangle: function(triangle = Rngon.ngon())
+        {
+            const ray = this;
+            const epsilon = 0.00001;
+            const noHit = Infinity;
+        
+            const e1 = Rngon.vector3((triangle.vertices[1].worldX - triangle.vertices[0].worldX),
+                                     (triangle.vertices[1].worldY - triangle.vertices[0].worldY),
+                                     (triangle.vertices[1].worldZ - triangle.vertices[0].worldZ));
+        
+            const e2 = Rngon.vector3((triangle.vertices[2].worldX - triangle.vertices[0].worldX),
+                                     (triangle.vertices[2].worldY - triangle.vertices[0].worldY),
+                                     (triangle.vertices[2].worldZ - triangle.vertices[0].worldZ));
+        
+            const pv = Rngon.vector3.cross(ray.dir, e2);
+            const det = Rngon.vector3.dot(e1, pv);
+            if ((det > -epsilon) && (det < epsilon)) return noHit;
+        
+            const invD = (1.0 / det);
+            const tv = Rngon.vector3((ray.pos.x - triangle.vertices[0].worldX),
+                                     (ray.pos.y - triangle.vertices[0].worldY),
+                                     (ray.pos.z - triangle.vertices[0].worldZ));
+            const u = (Rngon.vector3.dot(tv, pv) * invD);
+            if ((u < 0) || (u > 1)) return noHit;
+        
+            const qv = Rngon.vector3.cross(tv, e1);
+            const v = (Rngon.vector3.dot(ray.dir, qv) * invD);
+            if ((v < 0) || ((u + v) > 1)) return noHit;
+        
+            const distance = (Rngon.vector3.dot(e2, qv) * invD);
+            if (distance <= 0) return noHit; 
+        
+            return distance;
+        },
+
+        // Adapted from https://tavianator.com/fast-branchless-raybounding-box-intersections/.
+        intersect_aabb: function(aabb)
+        {
+            const ray = this;
+
+            const dirX = (1 / ray.dir.x);
+            const dirY = (1 / ray.dir.y);
+            const dirZ = (1 / ray.dir.z);
+
+            const tx1 = (aabb.min.x - ray.pos.x) * dirX;
+            const tx2 = (aabb.max.x - ray.pos.x) * dirX;
+            let tmin = Math.min(tx1, tx2);
+            let tmax = Math.max(tx1, tx2);
+
+            const ty1 = (aabb.min.y - ray.pos.y) * dirY;
+            const ty2 = (aabb.max.y - ray.pos.y) * dirY;
+            tmin = Math.max(tmin, Math.min(ty1, ty2));
+            tmax = Math.min(tmax, Math.max(ty1, ty2));
+
+            const tz1 = (aabb.min.z - ray.pos.z) * dirZ;
+            const tz2 = (aabb.max.z - ray.pos.z) * dirZ;
+            tmin = Math.max(tmin, Math.min(tz1, tz2));
+            tmax = Math.min(tmax, Math.max(tz1, tz2));
+
+            return (tmax >= 0 && tmax >= tmin);
+        },
+
+        // Traces the ray recursively through the given BVH. Returns null if no triangle in
+        // the BVH was intersected; and otherwise an object containing the triangle that was
+        // intersected and the distance to the point of intersection on it along the ray.
+        intersect_bvh: function(bvh)
+        {
+            const ray = this;
+
+            const intersectionInfo = {
+                triangle: null,
+                distance: Infinity,
+            };
+
+            (function trace(aabb)
+            {
+                if (aabb.isLeaf)
+                {
+                    for (const triangle of aabb.triangles)
+                    {
+                        const distance = ray.intersect_triangle(triangle);
+
+                        if (distance < intersectionInfo.distance)
+                        {
+                            intersectionInfo.triangle = triangle;
+                            intersectionInfo.distance = distance;
+                        }
+                    }
+
+                    return;
+                }
+
+                if (ray.intersect_aabb(aabb.mutable.left)) trace(aabb.mutable.left);
+                if (ray.intersect_aabb(aabb.mutable.right)) trace(aabb.mutable.right);
+            })(bvh.base);
+
+            return ((intersectionInfo.triangle === null)? null : intersectionInfo);
+        },
+    };
+
+    return publicInterface;
+}
---
+++ samples/raytraced-lighting/raytraced-lighting.js
@@ -0,0 +1,226 @@
+/*
+ * 2020 Tarpeeksi Hyvae Soft
+ *
+ * Software: Retro n-gon renderer / render sample
+ * 
+ * A render sample which showcases ray-traced lighting using pixel and vertex shaders.
+ * 
+ * NOTE: This sample only supports scenes made of triangles.
+ * 
+ * NOTE: This sample only supports static scenes.
+ * 
+ * 
+ * Here's how it works:
+ * 
+ * 1. A vertex shader is used to build a complete list of the scene's world-space
+ *    polygons.
+ * 
+ *    Although pixel shaders - which we'll use for the ray-tracing - also receive a list
+ *    of the scene's polygons, the polygons will at that point have been clipped and
+ *    culled against the viewport and so they don't represent the world outside of the
+ *    viewport. Vertex shaders, on the other hand, receive polygons in object space and
+ *    prior to clipping, so the entire scene is represented and compatible with
+ *    world-space ray-tracing.
+ * 
+ * 2. A BVH tree is built out of the world's polygons. This helps speed up the ray-tracing.
+ * 
+ * 3. A ray-tracing pixel shader casts a ray from each pixel's world coordinates toward
+ *    the light source. If the ray hits a polygon that's closer than the light source, we
+ *    consider the pixel to be in full shadow. Otherwise, we'll shade the pixel based on
+ *    its distance to the light source and the incident angle of the light.
+ * 
+ */
+
+"use strict";
+
+import {first_person_camera} from "./camera.js";
+import {scene} from "./assets/scene.rngon-model.js";
+import {bvh} from "./bvh.js";
+import {ray} from "./ray.js";
+
+const lights = [
+    // The light source we'll trace rays toward.
+    Rngon.light(Rngon.translation_vector(11, 45, -35), {intensity: 3, reach: 150}),
+];
+
+const camera = first_person_camera("canvas",
+{
+    position: {x:-70, y:33, z:-7},
+    direction: {x:7, y:90, z:0},
+    movementSpeed: 0.05,
+});
+
+scene.initialize();
+
+// The world-space polygons - and their BVH tree - that we'll use for ray-tracing.
+const worldSpacePolys = [];
+let sceneBVH = null;
+
+// Returns the n-gons of the scene to be rendered by the retro n-gon renderer.
+// This function gets called by the renderer once per frame.
+export const sample_scene = (frameCount = 0)=>
+{
+    // Once the vertex shader has finished building a list of the scene's world-space
+    // polygons, we can build a BVH tree for the ray tracer.
+    if (!sceneBVH && worldSpacePolys.length)
+    {
+        sceneBVH = bvh(worldSpacePolys);
+    }
+
+    // While ray tracing is disabled, we'll let the retro n-gon renderer use its
+    // built-in vertex shading.
+    for (const ngon of scene.ngons)
+    {
+        ngon.material.renderVertexShade = (parent.RAYTRACING_ENABLED? false : true);
+    }
+
+    camera.update();
+
+    // Assumes 'renderSettings' is a pre-defined global object from which the
+    // renderer will pick up its settings.
+    renderSettings.cameraDirection = camera.direction;
+    renderSettings.cameraPosition = camera.position;
+
+    // Move the light source around in a circle.
+    lights[0].position.x += Math.cos(frameCount / 35);
+    lights[0].position.z += Math.sin(frameCount / 35);
+
+    return Rngon.mesh(scene.ngons,
+    {
+        scaling: Rngon.scaling_vector(25, 25, 25)
+    });
+};
+
+// This scene's custom render options for the retro n-gon renderer.
+export const sampleRenderOptions = {
+    lights: lights,
+    get pixelShaderFunction()
+    {
+        return (parent.RAYTRACING_ENABLED? ps_raytraced_lighting : null);
+    },
+    get vertexShaderFunction()
+    {
+        // We'll only copy the scene's polygons once. Once the BVH has been built,
+        // we know the polygons have been copied and don't need to do it any more.
+        return (sceneBVH? null : vs_copy_ngons);
+    },
+}
+
+// A vertex shader that copies, one by one, the scene's world-space polygons into
+// a list. This function gets called for each of the scene's n-gons.
+function vs_copy_ngons(ngon)
+{
+    if (ngon.vertices.length != 3)
+    {
+        Rngon.throw("Detected an unsupported type of n-gon. Only triangles are supported.");
+        return;
+    }
+
+    const newNgon = Rngon.ngon([]);
+
+    for (let v = 0; v < ngon.vertices.length; v++)
+    {
+        newNgon.vertices[v] = Rngon.vertex(ngon.vertices[v].x,
+                                           ngon.vertices[v].y,
+                                           ngon.vertices[v].z,
+                                           ngon.vertices[v].u,
+                                           ngon.vertices[v].v,
+                                           ngon.vertices[v].w);
+    }
+
+    newNgon.material = ngon.material;
+    newNgon.normal.x = ngon.normal.x;
+    newNgon.normal.y = ngon.normal.y;
+    newNgon.normal.z = ngon.normal.z;
+    newNgon.isActive = true;
+    newNgon.mipLevel = ngon.mipLevel;
+
+    worldSpacePolys.push(newNgon);
+
+    return;
+}
+
+// A pixel shader that shades each pixel based on whether and how the light source is
+// visible to it.
+function ps_raytraced_lighting({renderWidth, renderHeight, fragmentBuffer, pixelBuffer})
+{
+    // Defer shading until the scene's BVH has been built.
+    if (!sceneBVH)
+    {
+        return;
+    }
+
+    const light = Rngon.internalState.lights[0];
+
+    // Pre-create storage objects, so we don't need to keep re-creating them in the
+    // render loop.
+    const lightDirection = Rngon.vector3();
+    const surfaceNormal = Rngon.vector3();
+    const pixelWorldPosition = Rngon.vector3();
+
+    for (let i = 0; i < (renderWidth * renderHeight); i++)
+    {
+        const thisFragment = fragmentBuffer[i];
+
+        if (!thisFragment)
+        {
+            continue;
+        }
+
+        pixelWorldPosition.x = thisFragment.worldX;
+        pixelWorldPosition.y = thisFragment.worldY;
+        pixelWorldPosition.z = thisFragment.worldZ;
+
+        // The pixel's world position is used as the ray's origin, so offset it to avoid
+        // self-intersection.
+        pixelWorldPosition.x += (surfaceNormal.x * 0.00001);
+        pixelWorldPosition.y += (surfaceNormal.y * 0.00001);
+        pixelWorldPosition.z += (surfaceNormal.z * 0.00001);
+
+        const lightDistance = Math.sqrt(((pixelWorldPosition.x - light.position.x) * (pixelWorldPosition.x - light.position.x)) +
+                                        ((pixelWorldPosition.y - light.position.y) * (pixelWorldPosition.y - light.position.y)) +
+                                        ((pixelWorldPosition.z - light.position.z) * (pixelWorldPosition.z - light.position.z)));
+        
+        const distanceMul = Math.max(0, Math.min(1, (1 - (lightDistance / light.reach))));
+
+        lightDirection.x = (light.position.x - thisFragment.worldX);
+        lightDirection.y = (light.position.y - thisFragment.worldY);
+        lightDirection.z = (light.position.z - thisFragment.worldZ);
+        Rngon.vector3.normalize(lightDirection);
+
+        surfaceNormal.x = thisFragment.normalX;
+        surfaceNormal.y = thisFragment.normalY;
+        surfaceNormal.z = thisFragment.normalZ;
+
+        const shadeMul = Math.max(0, Math.min(1, Rngon.vector3.dot(surfaceNormal, lightDirection)));
+
+        // If distanceMul * shadeMul is <= 0, it means there's no light falling on this
+        // pixel from the light source, and so we don't need to cast a light ray. Otherwise,
+        // we cast a ray from this pixel toward the light's direction.
+        const intersection = ((distanceMul * shadeMul > 0) &&
+                              ray(pixelWorldPosition, lightDirection).intersect_bvh(sceneBVH));
+
+        // If the light ray intersects nothing or intersects something that's closer
+        // than the light, it means light is prevented from reaching on this pixel.
+        // (Technically, if the ray intersects nothing, it means there can be nothing
+        // blocking the light from reaching the pixel, but for code simplicity we count
+        // non-intersections as blocking.)
+        if (!intersection ||
+            (intersection.distance < lightDistance))
+        {
+            pixelBuffer[(i * 4) + 0] = 0;
+            pixelBuffer[(i * 4) + 1] = 0;
+            pixelBuffer[(i * 4) + 2] = 0;
+        }
+        // Otherwise, at least some light is falling on this pixel, dependent on the
+        // light's distance and incident angle.
+        else
+        {
+            pixelBuffer[(i * 4) + 0] *= (distanceMul * shadeMul * light.intensity);
+            pixelBuffer[(i * 4) + 1] *= (distanceMul * shadeMul * light.intensity);
+            pixelBuffer[(i * 4) + 2] *= (distanceMul * shadeMul * light.intensity);
+        }
+    }
+
+    return;
+}
