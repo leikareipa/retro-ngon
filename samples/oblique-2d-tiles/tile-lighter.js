4ce1d3eecea98e9bcabfa8abccbd4d1ba364f36e
leikareipa <drilledicecream@gmail.com>
2021-02-25 04:56:03 +0200

    samples/oblique-2d-tiles: Add rudimentary lighting

---
+++ samples/oblique-2d-tiles/tile-lighter.js
@@ -0,0 +1,58 @@
+/*
+ * 2021 Tarpeeksi Hyvae Soft
+ *
+ * Software: Retro n-gon renderer
+ * 
+ * Reimplements the retro n-gon renderer's n-gon lighting code to be more suitable
+ * for lighting oblique 2D tiles.
+ *
+ */
+
+"use strict";
+
+export function tile_lighter(ngon)
+{
+    if (ngon.material.vertexShading !== "gouraud")
+    {
+        return;
+    }
+
+    for (let v = 0; v < ngon.vertices.length; v++)
+    {
+        ngon.vertices[v].shade = ngon.material.ambientLightLevel;
+    }
+
+    // Find the brightest shade falling on this n-gon's vertices.
+    for (const light of Rngon.internalState.lights)
+    {
+        for (let v = 0; v < ngon.vertices.length; v++)
+        {
+            const vertex = ngon.vertices[v];
+            let vX = vertex.x;
+            let vY = vertex.y;
+
+            // Rotate the vertex positions we'll use for calculating light rays.
+            // We do this since the tile graphics are rotated 45 degrees but the
+            // n-gons on which the graphics are displayed are squares interleaved
+            // to create the seamless tiled look - so tiles surrounding a tile on
+            // which a light source is placed would be brighter than the source
+            // tile as their vertex corners are closer to the light source.
+            switch (v)
+            {
+                case 0: vX += 5; break;
+                case 1: vY -= 5; break;
+                case 2: vX -= 5; break;
+                case 3: vY += 5; break;
+            }
+
+            const distance = Math.sqrt(((vX - light.position.x) * (vX - light.position.x)) +
+                                       ((vY - light.position.y) * (vY - light.position.y)));
+
+            const distanceMul = (1 / (1 + (light.attenuation * distance)));
+
+            vertex.shade = Math.max(vertex.shade, Math.min(light.clip, (distanceMul * light.intensity)));
+        }
+    }
+
+    return;
+}
