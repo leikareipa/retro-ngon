304c28411926f2679b0ab009284469f28bb1251f
leikareipa <drilledicecream@gmail.com>
2021-02-26 08:20:25 +0200

    samples/oblique-2d-tiles: Shade tiles in unit size
    Instead of computing a shade value for each of a tile's corners, we do
    it for the tile's unit-size base square. This effectively flattens all
    tiles (e.g. vertical walls) to the height of ground tiles, creating a
    more uniform shading across all tiles.
    
    Whether this is a better lighting strategy overall depends on the
    tilemap's topology (e.g. whether there are many stacked layers or tall
    walls), but it works better for this particular render sample.

--- samples/oblique-2d-tiles/tile-lighter.js
+++ samples/oblique-2d-tiles/tile-lighter.js
@@ -10,6 +10,9 @@
 
 "use strict";
 
+const groundTileWidth = 22;
+const groundTileHeight = 22;
+
 export function tile_lighter(ngon)
 {
     if (ngon.material.vertexShading !== "gouraud")
@@ -37,12 +40,16 @@ export function tile_lighter(ngon)
             // to create the seamless tiled look - so tiles surrounding a tile on
             // which a light source is placed would be brighter than the source
             // tile as their vertex corners are closer to the light source.
+            //
+            // We also modify the top (#0 and #3) vertices' Y values to create a
+            // unit tile, i.e. a tile as tall as wide, so that lighting tall tiles
+            // (e.g. walls) is uniform with how ground tiles (unit size) are lit.
             switch (v)
             {
-                case 0: vX += 5; break;
+                case 0: vY = (ngon.vertices[1].y - groundTileHeight); vX += 5; break;
                 case 1: vY -= 5; break;
                 case 2: vX -= 5; break;
-                case 3: vY += 5; break;
+                case 3: vY = (ngon.vertices[1].y - groundTileHeight); vY += 5; break;
             }
 
             const distance = Math.sqrt(((vX - light.position.x) * (vX - light.position.x)) +
