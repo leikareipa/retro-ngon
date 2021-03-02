5a66f8a3e433ede2eed7c4b9e1b3014d2b8a5274
leikareipa <drilledicecream@gmail.com>
2021-03-02 21:53:59 +0200

    samples/rotating-triangle: Add vertex shade
    Makes the triangle a bit more interesting to look at.

--- samples/rotating-triangle/rotating-triangle.js
+++ samples/rotating-triangle/rotating-triangle.js
@@ -11,20 +11,21 @@ export const sample_scene = (frameCount)=>
 {
     const rotationSpeed = 1;
 
-    const triangle = Rngon.ngon([Rngon.vertex(-0.5, -0.5, 0),
-                                 Rngon.vertex(0.5, -0.5, 0),
-                                 Rngon.vertex(0.5, 0.5, 0)],
+    const triangle = Rngon.ngon([Rngon.vertex(-30, -30, 0),
+                                 Rngon.vertex(30, -30, 0),
+                                 Rngon.vertex(30, 30, 0)],
                                 {
                                     color: Rngon.color_rgba(255, 255, 0),
                                     texture: null,
-                                    hasWireframe: true,
-                                    wireframeColor: Rngon.color_rgba(20, 20, 20),
                                 });
+
+    triangle.vertices[0].shade = 1;
+    triangle.vertices[1].shade = 0.5;
+    triangle.vertices[2].shade = 0.2;
                             
     return Rngon.mesh([triangle],
     {
         rotation: Rngon.rotation_vector(0, (-40 + rotationSpeed * frameCount), 0),
-        scaling: Rngon.scaling_vector(50, 50, 50)
     });
 };
 
