492ce721e8db8554a8cb14aa841d2287240531b8
leikareipa <drilledicecream@gmail.com>
2021-04-26 05:28:18 +0300

    samples: Remove dependency on PHP
    So the samples can now be hosted as static content.

--- samples/point-rendering/point-rendering.js
+++ samples/point-rendering/point-rendering.js
@@ -3,32 +3,6 @@
  *
  * Software: Retro n-gon renderer / render sample
  * 
- * A render sample which showcases ray-traced lighting using pixel and vertex shaders.
- * 
- * NOTE: This sample only supports scenes made of triangles.
- * 
- * NOTE: This sample only supports static scenes.
- * 
- * 
- * Here's how it works:
- * 
- * 1. A vertex shader is used to build a complete list of the scene's world-space
- *    polygons.
- * 
- *    Although pixel shaders - which we'll use for the ray-tracing - also receive a list
- *    of the scene's polygons, the polygons will at that point have been clipped and
- *    culled against the viewport and so they don't represent the world outside of the
- *    viewport. Vertex shaders, on the other hand, receive polygons in object space and
- *    prior to clipping, so the entire scene is represented and compatible with
- *    world-space ray-tracing.
- * 
- * 2. A BVH tree is built out of the world's polygons. This helps speed up the ray-tracing.
- * 
- * 3. A ray-tracing pixel shader casts a ray from each pixel's world coordinates toward
- *    the light source. If the ray hits a polygon that's closer than the light source, we
- *    consider the pixel to be in full shadow. Otherwise, we'll shade the pixel based on
- *    its distance to the light source and the incident angle of the light.
- * 
  */
 
 "use strict";
