5012970a901fcbf23995208ca2716d733f2f5f2a
leikareipa <drilledicecream@gmail.com>
2023-04-14 03:28:17 +0300

    Move some src files from core/ to api/

--- js/retro-ngon/retro-ngon.js
+++ js/retro-ngon/retro-ngon.js
@@ -7,25 +7,21 @@
 
 import "../paletted-canvas/paletted-canvas.js";
 
-import {render} from "./api/render.js";
-import {render_async} from "./api/render-async.js";
 import {rasterize} from "./pipeline/rasterize.js";
 import {surface_wipe} from "./pipeline/surface-wipe.js";
 import {transform_clip_light} from "./pipeline/transform-clip-light.js";
+import {render} from "./api/render.js";
+import {render_async} from "./api/render-async.js";
 import {
     color,
     color_index,
-} from "./core/color.js";
-import {state} from "./core/internal-state.js";
-import {light} from "./core/light.js";
-import {material} from "./core/material.js";
-import {matrix44} from "./core/matrix44.js";
-import {mesh} from "./core/mesh.js";
-import {ngon} from "./core/ngon.js";
-import {renderShared} from "./core/render-shared.js";
-import {surface} from "./core/surface.js";
-import {texture} from "./core/texture.js";
-import {trig} from "./core/trig.js";
+} from "./api/color.js";
+import {light} from "./api/light.js";
+import {material} from "./api/material.js";
+import {matrix44} from "./api/matrix44.js";
+import {mesh} from "./api/mesh.js";
+import {ngon} from "./api/ngon.js";
+import {texture} from "./api/texture.js";
 import {
     assert,
     $throw,
@@ -34,9 +30,13 @@ import {
     log,
     renderable_width_of,
     renderable_height_of,
-} from "./core/util.js";
-import {vector} from "./core/vector.js";
-import {vertex} from "./core/vertex.js";
+} from "./api/util.js";
+import {vector} from "./api/vector.js";
+import {vertex} from "./api/vertex.js";
+import {renderShared} from "./core/render-shared.js";
+import {surface} from "./core/surface.js";
+import {state} from "./core/internal-state.js";
+import {trig} from "./core/trig.js";
 
 export const Rngon = {
     version: {
