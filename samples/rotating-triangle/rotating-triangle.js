62b977a590a3a4428f3fddc74fd8a4be941e8acf
leikareipa <drilledicecream@gmail.com>
2023-08-26 03:18:09 +0300

    samples/rotating-triangle: Tweak presentation

--- samples/rotating-triangle/rotating-triangle.js
+++ samples/rotating-triangle/rotating-triangle.js
@@ -1,5 +1,5 @@
 /*
- * 2020 Tarpeeksi Hyvae Soft
+ * 2020-2023 Tarpeeksi Hyvae Soft
  *
  * Software: Retro n-gon renderer
  * 
@@ -8,32 +8,42 @@
 "use strict";
 
 export const sample = {
-    initialize: function(){},
+    initialize: async function()
+    {
+        this.triangle = Rngon.ngon([
+            Rngon.vertex(-30, -30, 0, 0, 0),
+            Rngon.vertex( 30, -30, 0, 1, 0),
+            Rngon.vertex( 30,  30, 0, 1, 1)], {
+                textureMapping: "affine",
+                isTwoSided: true,
+                color: Rngon.color.lightgray,
+                wireframeColor: Rngon.color.lightgray,
+            },
+            Rngon.vector(0, 0, -1),
+        );
+    },
     tick: function()
     {
         this.numTicks++;
-
-        const rotationSpeed = 1;
-
-        const triangle = Rngon.ngon([Rngon.vertex(-30, -30, 0),
-                                     Rngon.vertex(30, -30, 0),
-                                     Rngon.vertex(30, 30, 0)], {
-            color: Rngon.color(255, 255, 0),
-            texture: null,
-        });
-
-        triangle.vertices[0].shade = 1;
-        triangle.vertices[1].shade = 0.5;
-        triangle.vertices[2].shade = 0.2;
 
         return {
             renderOptions: {
                 useDepthBuffer: false,
             },
-            mesh: Rngon.mesh([triangle], {
-                rotate: Rngon.vector(0, (-40 + rotationSpeed * this.numTicks), 0),
+            renderPipeline: {
+                vertexShader: function(ngon)
+                {
+                    const isFacingAway = (Rngon.vector.dot(ngon.normal, Rngon.vector(0, 0, -1)) < 0);
+                    ngon.material.hasWireframe = isFacingAway;
+                    ngon.material.hasFill = !isFacingAway;
+                },
+            },
+            mesh: Rngon.mesh([this.triangle], {
+                rotate: Rngon.vector(0, (-40 + this.rotationSpeed * this.numTicks), 0),
             }),
         };
     },
+    rotationSpeed: 1,
+    triangle: undefined,
     numTicks: 0,
 };
