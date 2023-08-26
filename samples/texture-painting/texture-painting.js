2e44afe3f7ee034ee1b7f0ab6afb37afd9598c0c
leikareipa <drilledicecream@gmail.com>
2023-08-26 05:36:33 +0300

    samples: Add a texture painting sample

---
+++ samples/texture-painting/texture-painting.js
@@ -0,0 +1,119 @@
+/*
+ * 2023 Tarpeeksi Hyvae Soft
+ *
+ * Software: Retro n-gon renderer
+ * 
+ */
+
+export const sample = {
+    initialize: async function()
+    {
+        this.texture = await Rngon.texture.load("../samples/shared/textures/wood.rngon-texture.json");
+
+        {
+            const canvasEl = document.getElementById("canvas");
+            
+            canvasEl.addEventListener("mousemove", (event)=>{
+                if (this.mousePos)
+                {
+                    this.mousePos = {
+                        x: (event.clientX * this.renderScale - 0.5),
+                        y: (event.clientY * this.renderScale - 0.5),
+                    };
+                }
+            });
+         
+            canvasEl.addEventListener("mousedown", (event)=>{
+                this.mousePos = {
+                    x: (event.clientX * this.renderScale - 0.5),
+                    y: (event.clientY * this.renderScale - 0.5),
+                };
+            });
+         
+            canvasEl.addEventListener("mouseup", ()=>{
+                this.mousePos = this.prevMousePos = undefined;
+            });
+        }
+
+        this.triangle = Rngon.ngon([
+            Rngon.vertex(-40, -40, 0, 0, 0),
+            Rngon.vertex( 40, -40, 0, 1, 0),
+            Rngon.vertex( 40,  40, 0, 1, 1)], {
+                textureMapping: "affine",
+                isTwoSided: true,
+                isTwoSided: true,
+
+                texture: this.texture,
+            },
+            Rngon.vector(0, 0, -1),
+        );
+    },
+    tick: function(defaultRenderOptions)
+    {
+        this.renderScale = defaultRenderOptions.resolution;
+        this.numTicks++;
+
+        if (this.mousePos)
+        {
+            paint.call(this);
+            this.prevMousePos = this.mousePos;
+        }
+        return {
+            mesh: Rngon.mesh([this.triangle]),
+            renderOptions: {
+                useDepthBuffer: false,
+                useFragmentBuffer: true,
+                fragments: {
+                    textureUScaled: true,
+                    textureVScaled: true,
+                },
+            },
+        };
+    },
+    mousePos: undefined,
+    prevMousePos: undefined,
+    renderScale: 1,
+    rotationSpeed: 0.5,
+    triangle: undefined,
+    texture: undefined,
+    numTicks: 0,
+};
+
+// Draws a line into the texture between the previous and current mouse positions.
+function paint()
+{
+    if (!this.mousePos || !this.prevMousePos)
+    {
+        return;
+    }
+
+    const {x:startX, y:startY} = this.prevMousePos;
+    const {x:endX, y:endY} = this.mousePos;
+    
+    let lineLength = Math.ceil(Math.sqrt((endX - startX)**2 + (endY - startY)**2));
+    const deltaX = ((endX - startX) / lineLength);
+    const deltaY = ((endY - startY) / lineLength);
+
+    let realX = startX;
+    let realY = startY;
+    while (lineLength--)
+    {
+        const x = ~~realX;
+        const y = ~~realY;
+        const fragmentIdx = (x + y * Rngon.state.default.fragmentBuffer.width);
+        const {textureUScaled:texelU, textureVScaled:texelV} = Rngon.state.default.fragmentBuffer.data[fragmentIdx];
+
+        if (![typeof texelU, typeof texelV].includes("undefined"))
+        {
+            const texelIdx = (texelU + texelV * this.texture.width);
+            this.texture.pixels[texelIdx*4+0] = 255;
+            this.texture.pixels[texelIdx*4+1] = 255;
+            this.texture.pixels[texelIdx*4+2] = 255;
+        }
+
+        realX += deltaX;
+        realY += deltaY;
+    }
+
+    this.texture.refresh();
+}
