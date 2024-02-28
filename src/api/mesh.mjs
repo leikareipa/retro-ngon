f9e9b22e43a1a9942c9a75b95a4236f6533ecd3b
leikareipa <drilledicecream@gmail.com>
2024-02-28 14:23:51 +0200

    api/mesh: Attach default transform params by value, not reference
    So modifying the default-initialized transform params of a mesh doesn't
    modify every default-initialized mesh's transform params.

--- src/api/mesh.mjs
+++ src/api/mesh.mjs
@@ -67,7 +67,7 @@ export function Mesh(
     const publicInterface = {
         $constructor: "Mesh",
         ngons,
-        ...meshDefaultTransform,
+        ...structuredClone(meshDefaultTransform),
         ...transform
     };
 
